dedbcdb43c3b551584adceb60c45ac3d0667782b
Xiuheng <wuxhme@gmail.com>
2021-06-19 20:17:51 +0800

    Still need libgomp1 for souffle

--- server/build-run.sh
+++ server/build-run.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 docker build -t evome-server .
-docker run -d --rm --name evome-server -p 3000:3000 evome-server
+# docker run -d --rm --name evome-server -p 3000:3000 evome-server
+docker run --rm --name evome-server -p 3000:3000  -v /home/ec2-user/.local/repo-storage:/repo-storage  evome-server
+
